4c7a2989e8cc462fb71537db3e364fe65fab9fb0
labixiaoxin12345 <vivianleeching@hotmail.com>
2024-02-03 00:08:29 +0800

    added 5 classes(Carpark, ParkingRecord, StatusState, User, Vehicle) including subclasses(Student, Staff, Admin, Active, Expired, Terminated)

---
+++ SE_ASG/Models/Carpark.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE_ASG.Models
+{
+    public class Carpark
+    {
+        private string carParkNumber { get; set; }
+        private string description { get; set; }
+        private string location { get; set; }
+
+
+    }
+}
---
+++ SE_ASG/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE_ASG.Models
+{
+    public class User
+    {
+        private string name { get; set; }
+        private int userId { get; set; }
+        private string username { get; set; }
+        private string password { get; set; }
+        private int mobileNumber { get; set; }
+
+    }
+    public class Student : User{ }
+    public class Staff : User{ }
+    public class Admin : User{ }
+
+}
