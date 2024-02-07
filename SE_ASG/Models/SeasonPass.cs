4f4d832b3a48af77e95c619bccb5df1e6e04b836
yexuan1 <s10242695@connect.np.edu.sg>
2024-02-07 18:55:13 +0800

    seasonPass update

--- SE_ASG/Models/SeasonPass.cs
+++ SE_ASG/Models/SeasonPass.cs
@@ -3,80 +3,94 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-// Define the interface for the strategy
-public interface ICalculateCharge
+namespace SE_ASG.Models
 {
-    decimal CalculateCharge();
-}
+
+    // Define the interface for the strategy
+    public interface ICalculateCharge
+    {
+        decimal CalculateCharge();
+    }
 
     // Define the context class
     public class SeasonParking
     {
         public int PassNumber { get; set; }
 
-        public StatusState Status { get; set; }
+        public StatusState status;
+
+        public StatusState active;
+
+        public StatusState expired;
+
+        public StatusState terminated;
 
         private ICalculateCharge _calChargeStrategy;
 
         public SeasonParking(ICalculateCharge calChargeStrategy)
         {
             _calChargeStrategy = calChargeStrategy;
+            active = new Active(this);
+            expired = new Expired(this);
+            terminated = new Terminated(this);
+
+            status = active;
         }
 
         public void setState(StatusState status)
         {
-            Status = status;
+            this.status = status;
         }
 
         public decimal CalculateCharge()
         {
             return _calChargeStrategy.CalculateCharge();
         }
-}
-
-
-public enum StatusState
-{
-    Active,
-    Expired,
-    Terminated
-}
-
-
-public class MonthlySeasonPass : ICalculateCharge
-{
-    private DateTime StartDate { get; set; }
-    private DateTime EndDate { get; set; }
-    private int AvailablePasses { get; set; }
-
-        public decimal CalculateCharge()
-        {
-
-            return 0; // placeholder
-        }
     }
 
-// Define NoPass subclass
-public class NoPass : ICalculateCharge
-{
-    public decimal CalculateCharge()
+
+    public enum StatusState
     {
-      
-        return 0; // placeholder
+        Active,
+        Expired,
+        Terminated
     }
-}
 
-// Define DailySeasonPass subclass
-public class DailySeasonPass : ICalculateCharge
-{
-    private DateTime DateOfIssue { get; set; }
-    private decimal MaxDailyRate { get; set; }
+
+    public class MonthlySeasonPass : ICalculateCharge
+    {
+        private DateTime StartDate { get; set; }
+        private DateTime EndDate { get; set; }
+        private int AvailablePasses { get; set; }
 
         public decimal CalculateCharge()
         {
 
             return 0; // placeholder
         }
+    }
+
+    // Define NoPass subclass
+    public class NoPass : ICalculateCharge
+    {
+        public decimal CalculateCharge()
+        {
+
+            return 0; // placeholder
+        }
+    }
+
+    // Define DailySeasonPass subclass
+    public class DailySeasonPass : ICalculateCharge
+    {
+        private DateTime DateOfIssue { get; set; }
+        private decimal MaxDailyRate { get; set; }
+
+        public decimal CalculateCharge()
+        {
+
+            return 0; // placeholder
+        }
+    }
+
 }
-
-
--- SE_ASG/Models/StatusState.cs
+++ SE_ASG/Models/StatusState.cs
@@ -20,9 +20,9 @@ namespace SE_ASG.Models
     public class Active : StatusState
     {
         private bool parked { get; set; }
-        
+
         private SeasonParking myParking;
-        
+
         public Active(SeasonParking parking)
         {
             myParking = parking;
@@ -31,95 +31,90 @@ namespace SE_ASG.Models
         public void TerminatePass(string reason)
         {
             Console.WriteLine("You have selected Terminate Pass.");
-            myParking.setState(terminated);
-            
-        }
-
-        public void TransferPass(Vehicle vehicle) 
-        {
-        
-        }
-
-        public void MakePayment() 
-        {
-        
-        }
-
-        public void RenewPass()
-        {
-
-        }
-
-        public static implicit operator global::StatusState(Active v)
-        {
-            throw new NotImplementedException();
-        }
-    }
-    public class Expired : StatusState 
-    {
-        private SeasonParking seasonParking;
-
-        public Expired(SeasonParking seasonParking)
-        {
-            this.seasonParking = seasonParking;
-        }
-
-        public void TerminatePass(string reason)
-        {
+            myParking.setState(myParking.terminated);
 
         }
 
         public void TransferPass(Vehicle vehicle)
         {
-
+            //Implementation
+            myParking.setState(myParking.active);
         }
 
         public void MakePayment()
         {
-
+            //Implementation
         }
 
         public void RenewPass()
         {
-
+            //Implementation
+            myParking.setState(myParking.active);
         }
 
-        public static implicit operator global::StatusState(Expired v)
+    }
+    public class Expired : StatusState
+    {
+        private SeasonParking myParking;
+
+        public Expired(SeasonParking parking)
         {
-            throw new NotImplementedException();
+            myParking = parking;
+        }
+
+        public void TerminatePass(string reason)
+        {
+            //Implementation
+        }
+
+        public void TransferPass(Vehicle vehicle)
+        {
+            //Implementation
+        }
+
+        public void MakePayment()
+        {
+            //Implementation
+        }
+
+        public void RenewPass()
+        {
+            //Implementation
         }
     }
 
     public class Terminated : StatusState
     {
-        public void GenerateRefund() 
+        private SeasonParking myParking;
+
+        public Terminated(SeasonParking parking)
         {
-        
+            myParking = parking;
+        }
+
+        public void GenerateRefund(float amount)
+        {
+            //Implementation
         }
 
         public void TerminatePass(string reason)
         {
-
+            //Implementation
         }
 
         public void TransferPass(Vehicle vehicle)
         {
-
+            //Implementation
         }
 
         public void MakePayment()
         {
-
+            //Implementation
         }
 
         public void RenewPass()
         {
-
-        }
-
-        public static implicit operator global::StatusState(Terminated v)
-        {
-            throw new NotImplementedException();
+            //Implementation
         }
     }
 }
